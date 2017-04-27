8745ee6de3c5ae8c10fe1b28ed98f1201a97332f
Nate Pond <nathaniel.p.pond@gmail.com>
2017-04-27 14:58:41 -0400

    second commmit

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.twitterKeys = {
+    consumer_key: 'E5PXXSAkRVN5TSfmJ6jKXTONG',
+    consumer_secret: '6u0NoTpDH7spti0IHBoIV2kCuI0PcBRq3ue2gsUOGNYxjGA068',
+    access_token_key: '310732481-dGMGpzC50hsB5d57A2A08CmjJKIsukiosEmvZE4V',
+    access_token_secret: 'dUZ4UMDLzTstLssYegbpGXqp8XC7MUwX6GMSqwC1EsL9o',
+
+}
